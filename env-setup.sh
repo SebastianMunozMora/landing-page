9e14f39d044da1f9f7ffdeca4b180f30da1933fd
Sebastian <smm9308@gmail.com>
2025-02-07 11:27:29 -0500

    add(environment): prometheus

--- env-setup.sh
+++ env-setup.sh
@@ -37,7 +37,7 @@ prometheusData() {
     # Create persistent volume for your data
     sudo docker volume create prometheus-data
     # Start Prometheus container
-    docker run \
+    sudo docker run \
         -p 9090:9090 \
         -v /path/to/prometheus.yml:/etc/prometheus/prometheus.yml \
         -v prometheus-data:/prometheus \
